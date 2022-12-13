589c285ca53101964795e7316ca896bc506bd2af
Daniil T <>
2022-12-13 14:28:26 +0700

    MethodResult model added

---
+++ WeatherApp.Domain/Models/MethodResult.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WeatherApp.Domain.Models
+{
+    public class MethodResult<T>
+    {
+        public T Value { get; set; }
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+        public string Message { get; set; }
+        public string ExceptionMessage { get; set; }
+
+        private Exception _exception { get; set; }
+
+        public MethodResult()
+        {
+            Success = false;
+            Message = "Undefined";
+        }
+
+        public MethodResult<T> SetException(Exception ex)
+        {
+            _exception = ex;
+            GetFullExceptionMessage();
+            return this;
+        }
+        public MethodResult<T> SetValue(T value)
+        {
+            Value = value;
+            return this;
+        }
+        public Exception GetException()
+        {
+            return _exception;
+        }
+        private string GetFullExceptionMessage(Exception ex)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (ex == null)
+                return stringBuilder.ToString();
+            stringBuilder.AppendFormat("Message: {0}; Error body: {1}", Message, ex.Message);
+            if (ex.InnerException != null)
+                stringBuilder.AppendFormat("\r\nInner exception: {0}", GetFullExceptionMessage(ex.InnerException));
+            return stringBuilder.ToString();
+        }
+        public string GetFullExceptionMessage()
+        {
+            if (_exception == null)
+                return string.Format("Message: {0}; Exception missing", Message);
+            return string.Format("Description:{0}\r\nTrace:{1}", GetFullExceptionMessage(_exception), _exception.StackTrace);
+        }
+
+        public MethodResult<T> SetSuccess(T value, string message = "")
+        {
+            _exception = null;
+            Success = true;
+            Message = message;
+            Value = value;
+            return this;
+        }
+        public MethodResult<T> SetError(string message, Exception ex = null)
+        {
+            _exception = ex;
+            Success = false;
+            Message = message;
+            GetFullExceptionMessage();
+            return this;
+        }
+
+        public MethodResult<T> SetErrorList(List<string> listErrors)
+        {
+            Errors = listErrors;
+            return this;
+        }
+
+        public void ThrowExceptionIfNotOk()
+        {
+            if (Success)
+                return;
+            ThrowException();
+        }
+
+        public void ThrowException()
+        {
+            throw _exception == null ? new Exception() : new Exception(Message);
+        }
+
+        public static MethodResult<T> GetErrorResult(string message = "", Exception ex = null)
+        {
+            MethodResult<T> methodResult = new MethodResult<T>();
+            methodResult._exception = ex;
+            methodResult.Success = false;
+            methodResult.Message = message;
+            methodResult.GetFullExceptionMessage();
+            return methodResult;
+        }
+
+        public static MethodResult<T> GetExceptionResult(string message, Exception ex)
+        {
+            MethodResult<T> methodResult = new MethodResult<T>();
+            methodResult._exception = ex;
+            methodResult.Success = false;
+            methodResult.Message = message;
+            methodResult.Errors = TryGetErrors(ex);
+            methodResult.ExceptionMessage = ex.Message;
+            methodResult.GetFullExceptionMessage();
+            return methodResult;
+        }
+
+        public static MethodResult<T> GetSuccessResult(T value, string message = "")
+        {
+            return new MethodResult<T>()
+            {
+                _exception = null,
+                Success = true,
+                Message = message,
+                Value = value
+            };
+        }
+
+        private static List<string> TryGetErrors(Exception ex)
+        {
+            if (ex is ValidationException newException)
+            {
+                //return newException.Errors.Select(it => it.ErrorMessage).ToList();
+                return new List<string> { newException.Source };
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
---
+++ WeatherApp.Domain/Models/MethodResultExt.cs
@@ -0,0 +1,39 @@
+﻿namespace WeatherApp.Domain.Models
+{
+    public static class MethodResultExt
+    {
+        public static MethodResult<T> ToSuccessMethodResult<T>(this T result)
+        {
+            return MethodResult<T>.GetSuccessResult(result, string.Empty);
+        }
+
+        public static MethodResult<T> ToErrorMethodResult<T>(this Exception ex, string friendlyText = "An error has occurred")
+        {
+            return MethodResult<T>.GetExceptionResult(friendlyText, ex);
+        }
+
+        public static MethodResult<T> ToErrorMethodResult<T>(this string friendlyText)
+        {
+            return MethodResult<T>.GetErrorResult(friendlyText, (Exception)null);
+        }
+
+        public static MethodResult<T> ToErrorMethodResult<T>(this List<string> errors, Exception ex = null, string friendlyText = "An error has occurred")
+        {
+            MethodResult<T> methodResult = new MethodResult<T>();
+            methodResult.SetError(friendlyText, ex);
+            methodResult.Errors = errors;
+            return methodResult;
+        }
+
+        public static MethodResult<TTarget> To<TFrom, TTarget>(this MethodResult<TFrom> methodResultFrom, TTarget newValue)
+        {
+            MethodResult<TTarget> methodResult = new MethodResult<TTarget>();
+            methodResult.Success = methodResultFrom.Success;
+            methodResult.Value = newValue;
+            methodResult.SetException(methodResultFrom.GetException());
+            methodResult.Errors = methodResultFrom.Errors;
+            methodResult.Message = methodResultFrom.Message;
+            return methodResult;
+        }
+    }
+}
