ee39f13878b9a2270e0b3bb5f749d402d6038c81
Daniil T <>
2022-12-13 14:31:03 +0700

    Any fixes

--- WeatherApp/Controllers/WeatherForecastController.cs
+++ WeatherApp/Controllers/WeatherForecastController.cs
@@ -8,36 +8,18 @@ namespace WeatherApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-        private readonly ILogger<WeatherForecastController> _logger;
         private readonly IWeatherService _weatherService;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherService weatherService)
+        public WeatherForecastController(IWeatherService weatherService)
         {
-            _logger = logger;
             _weatherService = weatherService;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("GetWeatherByZip")]
+        public async Task<CityTemperatureDto> GetByZipCode(string zipCode, string countryCode = "us")
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-        [HttpGet("GetWeatherByZipCodeAndCountryCode")]
-        public Task<CityTemperatureDto> GetByZipCode(string zipCode, string countryCode)
-        {
-            return _weatherService.GetCityTemperature(zipCode, countryCode);
+            return await _weatherService.GetCityTemperature(zipCode, countryCode);
         }
 
 
