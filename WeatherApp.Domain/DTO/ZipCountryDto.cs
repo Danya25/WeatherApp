9db8f01448f863454ac1fa4135f6ae920d6558ec
Daniil T <>
2022-12-13 17:17:33 +0700

    Add zip country dto

---
+++ WeatherApp.Domain/DTO/ZipCountryDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApp.Domain.DTO
+{
+    public class ZipCountryDto
+    {
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        [DefaultValue("us")]
+        public string CountryCode { get; set; } = "us";
+    }
+}
