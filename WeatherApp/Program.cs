c4dd1662c086dc2fb7bf043060c485475daaa030
Daniil T <>
2022-12-13 14:30:55 +0700

    Add cors

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,4 +1,5 @@
 using WeatherApp.Domain.Settings;
+using WeatherApp.Middleware;
 using WeatherApp.Services.Interfaces;
 using WeatherApp.Services.Weather;
 
@@ -12,21 +13,29 @@ builder.Services.AddHttpClient();
 
 builder.Services.Configure<WeatherSettings>(builder.Configuration.GetSection(nameof(WeatherSettings)));
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("Cors", builder =>
+    {
+        builder.SetIsOriginAllowed(_ => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+});
 
 builder.Services.AddSingleton<IWeatherService, WeatherService>();
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseCors("Cors");
 
 app.UseAuthorization();
+
+app.UseMiddleware<ApiExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
